49ebb8f01f9d0002ea61be478870e53b2f831b10
Urban Koistinen <chess@teo.se>
2023-01-07 17:28:17 +0100

    Parsing all FEN fields

--- chess.nim
+++ chess.nim
@@ -8,32 +8,39 @@ proc file*(sq: Square):int=sq.int and 7
 proc sq2str* (sq: Square):string =
   result.add("abcdefgh"[sq.file])
   result.add("12345678"[sq.rank])
-
-type Bitboard* = uint64
-proc bitboard* (sq: Square):Bitboard = 1u64 shl sq
-proc bb2str* (bb: Bitboard): string =
+proc str2sq(s: string): Square = s[0].ord-'a'.ord+8*(s[1].ord-'1'.ord)
+  
+type BB* = uint64
+proc bb* (sq: Square):Bb = 1u64 shl sq
+proc bb2str* (b: Bb): string =
   for rank in countdown(7,0):
     if rank<7: result.add('\n')
     for file in countup(0,7):
-      if 0 == (bb.Bitboard and bitboard(square(file, rank))):
+      if 0 == (b.Bb and bb(square(file, rank))):
         result.add('-')
       else:
         result.add('+')
 
 type Position* = object # 8*8 bytes
-  so: array[0..1, Bitboard]
-  pawns, knights, bishops, rooks, queens: Bitboard
+  so: array[0..1, BB]
+  pawns, knights, bishops, rooks, queens: BB
   game50: uint16
   halfmoves: uint16
   kings: array[0..1, uint8]
   ep: uint8
   castling: uint8
 
+proc castling2string(castling: uint8): string =
+  if 0 == castling: result = "-"
+  for i in countup(0,3):
+    if 0 < (castling and (8u shr i)):
+      result.add("KQkq"[i])
+
 proc `$`*(p: Position): string =
-  var oc: Bitboard = p.so[0] or p.so[1]
+  var oc: BB = p.so[0] or p.so[1]
   for rank in countdown(7,0):
     for file in countup(0,7):
-      var sq: Bitboard = bitboard(square(file, rank))
+      var sq: BB = bb(square(file, rank))
       if 0 == (oc and sq):
         result.add(if 1 == (1 and (rank+file)): '-' else: ' ')
       else:
@@ -53,9 +60,9 @@ proc `$`*(p: Position): string =
   result.add($p.halfmoves)
   result.add(" epsquare: ")
   if 0 < p.ep: result.add(p.ep.sq2str)
-  else: result.add("no")
+  else: result.add("-")
   result.add(" castling: ")
-  result.add(p.castling.BiggestInt.toBin(4))
+  result.add(castling2string(p.castling))
 
 proc emptyPosition: Position =
   result.so[0] = 0
@@ -70,31 +77,30 @@ proc emptyPosition: Position =
   result.kings[0] = 64 # missing white king is illegal position
   result.kings[1] = 64 # missing black king is illegal position
   result.ep = 0
-  result.castling = 0xf
+  result.castling = 0
   
 proc addPiece*(p: Position, piece: char, sq: Square): Position =
   result = p
-  var bb: Bitboard = bitboard(sq)
+  var b: BB = bb(sq)
   var side: int = if isLowerAscii(piece): 1 else: 0
-  result.so[side] = p.so[side] or bb
+  result.so[side] = p.so[side] or b
   case piece.toLowerAscii
-  of 'p': result.pawns = p.pawns or bb
-  of 'n': result.knights = p.knights or bb
-  of 'b': result.bishops = p.bishops or bb
-  of 'r': result.rooks = p.rooks or bb
-  of 'q': result.queens = p.queens or bb
+  of 'p': result.pawns = p.pawns or b
+  of 'n': result.knights = p.knights or b
+  of 'b': result.bishops = p.bishops or b
+  of 'r': result.rooks = p.rooks or b
+  of 'q': result.queens = p.queens or b
   of 'k':
     assert result.kings[side] == 64 # no previous king
     result.kings[side] = sq.uint8
   else: assert false
-  
 
-proc fen(s: string): Position =
+proc fen2p(s: string): Position =
   result = emptyPosition()
-  var fenArgs: string = s
+  var a: seq[string] = split(s)
   var rank: int = 7
   var file: int = 0
-  for c in fenArgs[0]:
+  for c in a[0]:
     assert file < 8 or c == '/'
     case c
     of '/':
@@ -107,20 +113,39 @@ proc fen(s: string): Position =
       result = result.addPiece(c, square(file, rank))
       inc file
     else: assert false
-    
+  if a[1] == "b":
+    result.halfmoves = 1
+  else:
+    assert "w" == a[1]
+  for c in a[2]:
+    case c
+    of '-':
+      result.castling = 0
+    of 'K':
+      result.castling = 8 or result.castling
+    of 'Q':
+      result.castling = 4 or result.castling
+    of 'k':
+      result.castling = 2 or result.castling
+    of 'q':
+      result.castling = 1 or result.castling
+    else: assert false
+  if "-" == a[3]: result.ep = 0
+  else: result.ep = str2sq(a[3]).uint8
+  result.game50 = a[4].parseInt.uint16
+  result.halfmoves = result.halfmoves + 2*a[5].parseInt.uint16
 
 proc startingPosition: Position =
-  fen("rnbqkbnr/pppppppp/////PPPPPPPP/RNBQKBNR")
-      
+  fen2p("rnbqkbnr/pppppppp/////PPPPPPPP/RNBQKBNR w KQkq - 0 0")
+
 type Move* = object
   fr: uint8 # 64..127 promotion, 128..191 ep, 192..256 castling
   to: uint8 # high 2 bits say promotion piece in case of promotion
 
 proc isPromotion(mv: Move):bool = 64 <= mv.fr and mv.fr <= 127
 
-  
 when isMainModule:
-  echo (bitboard(square(0,1)) or bitboard(square(2,2))).bb2str
+  echo (bb(square(0,1)) or bb(square(2,2))).bb2str
   echo square(0,1).sq2str
   echo square(2,2).sq2str
   var mv: Move
@@ -129,6 +154,6 @@ when isMainModule:
   echo isPromotion(mv)
   var p = startingPosition()
   echo p
-  p = fen("8/p7/1P6/1r3p1k/7P/3R1KP1/8/8") # b - - 0 0
+  p = fen2p("8/p7/1P6/1r3p1k/7P/3R1KP1/8/8 b - - 0 0")
   echo "8/p7/1P6/1r3p1k/7P/3R1KP1/8/8 b - - 0 0"
   echo p
